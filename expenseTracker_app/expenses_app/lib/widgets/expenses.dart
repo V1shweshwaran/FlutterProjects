34ed81ead13182a4c3cc1e4528991cb9dfbbbab5
Vishweshwaran S <vishweshsiva@gmail.com>
2024-08-13 19:05:38 +0530

    Added projects

---
+++ expenseTracker_app/expenses_app/lib/widgets/expenses.dart
@@ -0,0 +1,61 @@
+import 'package:expenses_app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+import 'package:expenses_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses_app/model/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+  a() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return const NewExpense();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker app'),
+        actions: [
+          IconButton(
+            onPressed: a,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}

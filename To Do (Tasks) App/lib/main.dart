57e153f537d7a119ce8c121c1c5bc94002bc986a
Vishweshwaran S <vishweshsiva@gmail.com>
2024-11-15 11:43:29 +0530

    commit

---
+++ To Do (Tasks) App/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list_app/myhomepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ToDoApp',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.redAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'ToDoApp'),
+    );
+  }
+}
---
+++ To Do (Tasks) App/lib/task.dart
@@ -0,0 +1,6 @@
+class Task {
+  String? title;
+  String? description;
+  bool? isCompleted;
+  Task({this.title, this.description, this.isCompleted = false});
+}
